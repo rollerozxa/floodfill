6f721c425b5628b35b323eb2161038e452dc2c36
ROllerozxa <rollerozxa@voxelmanip.se>
2025-04-10 21:54:50 +0200

    Make icon texture linearly scaled

--- src/textures.c
+++ src/textures.c
@@ -15,11 +15,15 @@ static bool textures_loaded = false;
 	texture_bank[id] = SDL_CreateTextureFromSurface(renderer, SDL_LoadQOI_IO(SDL_IOFromMem(data, len))); \
 	SDL_SetTextureScaleMode(texture_bank[id], SDL_SCALEMODE_NEAREST)
 
+#define LOAD_TEX_LINEAR(id, data, len) \
+	texture_bank[id] = SDL_CreateTextureFromSurface(renderer, SDL_LoadQOI_IO(SDL_IOFromMem(data, len))); \
+	SDL_SetTextureScaleMode(texture_bank[id], SDL_SCALEMODE_LINEAR)
+
 void textures_init(SDL_Renderer *renderer) {
 	LOAD_TEX(TEX_BG_STRIPES, background_stripes_qoi, background_stripes_qoi_len);
 	LOAD_TEX(TEX_CHECK, check_qoi, check_qoi_len);
 	LOAD_TEX(TEX_CUTE, cute_qoi, cute_qoi_len);
-	LOAD_TEX(TEX_ICON, icon_qoi, icon_qoi_len);
+	LOAD_TEX_LINEAR(TEX_ICON, icon_qoi, icon_qoi_len);
 	LOAD_TEX(TEX_PAUSE, pause_qoi, pause_qoi_len);
 
 	textures_loaded = true;
